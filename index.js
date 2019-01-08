bd3694dc46aad41dbd3fff98d5a72c5bd6475502
awesome-edition-6075 <zachpardee@gmail.com>
2019-01-08 22:49:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
 
   // Feel free to move things around!
   const two = 2
+  
+  return n + two;
 }
 
 var funkyFunction = function() {
